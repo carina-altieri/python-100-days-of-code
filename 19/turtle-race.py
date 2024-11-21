b0483c8e4ad749d31db7fbd5739fec91a019a6fa
Carina Altieri <carinaaltieri.08@gmail.com>
2024-11-21 12:02:23 -0300

    dia 20

--- 19/turtle-race.py
+++ 19/turtle-race.py
@@ -8,7 +8,7 @@ screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Who will win the race? Enter a color: ")
 turtle_colors = ["red", "orange", "blue", "green", "yellow", "purple"]
-y_position= [-80, -50, -20, 10, 40, 70]
+y_position= [-80, -50, -20, 10, 40, 70]  
 turtles = []
 
 for turtle in range(6):
@@ -17,8 +17,7 @@ for turtle in range(6):
     t.penup()
     t.goto(x=-230, y=y_position[turtle])
     turtles.append(t)
-
-
+    
 if user_bet:
     race_on = True
 
@@ -29,10 +28,8 @@ while race_on:
             winner_color = turtle.pencolor()
             if winner_color == user_bet:
                 print(f"You won! The {winner_color} turtle is the winner!")
-                
             else:
                 print(f"You lost! The {winner_color} turtle is the winner!")
-
         distance = random.randint(0,10)
         turtle.forward(distance)
 
---
+++ 20/snake.py
@@ -0,0 +1,50 @@
+from turtle import Turtle
+X_POSITION_START = [0, -20, -40]  # or: STARTING_POSITIONS = [(0, 0), (0, -20), (0, -40)]
+MOVE_FORWARD = 15
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+    def __init__(self):
+        # criando o atributo e chamando método create_snake
+        self.segments = []
+        self.create_snake()
+        self.snake_head = self.segments[0]
+
+    def create_snake(self): # toda a vez que for criado um objeto, essa função será executada
+        for snake_segment in range(3):
+            new_segment = Turtle(shape="square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(x=X_POSITION_START[snake_segment],y=0)
+            self.segments.append(new_segment)
+
+    def move(self):
+        for seg_num in range(len(self.segments) -1, 0, -1): # inicia pelo último segmento da cobra e vai até o índice 1 (não inclui o 0)
+            # pega as coordenadas dos segmentos à frente
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            # atualiza a posição do segmento atual
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_FORWARD)
+
+    # a ordem inversa é necessária porque cada segmento precisa assumir a posição do segmento imediatamente anterior antes que o anterior se mova.
+
+    def up(self):
+        if self.snake_head.heading() != DOWN:
+            self.snake_head.setheading(UP)
+
+    def down(self):
+        if self.snake_head.heading() != UP:
+            self.snake_head.setheading(DOWN)
+    
+    def left(self):
+        if self.snake_head.heading() != RIGHT:
+            self.snake_head.setheading(LEFT)
+    
+    def right(self):
+        if self.snake_head.heading() != LEFT:
+            self.snake_head.setheading(RIGHT)
+
